e9cf33f9d03226a24ef67fd99068b8d49d42bb0f
Jennie Ohyoung <johyoung@Jennies-MacBook-Pro.local>
2014-05-12 15:31:00 -0700

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Jennie Ohyoung on 5/12/14.
+//  Copyright (c) 2014 crittercism. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
