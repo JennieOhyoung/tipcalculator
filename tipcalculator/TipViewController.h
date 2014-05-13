e1631827fe57d0561d0abec71a5ba4b868d3278d
Jennie Ohyoung <johyoung@Jennies-MacBook-Pro.local>
2014-05-12 17:48:00 -0700

    first commit

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Jennie Ohyoung on 5/12/14.
+//  Copyright (c) 2014 crittercism. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
